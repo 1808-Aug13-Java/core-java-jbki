d05f17fe23d6354d419014bc6d5d7f2cdda0eb57
Jerry <jbki@protonmail.com>
2018-08-15 01:08:10 -0400

    reorganized file structure; added Exceptions

--- FourPillars/com/revature/main/Demo.java
+++ FourPillars/com/revature/main/Demo.java
@@ -13,25 +13,37 @@ public class Demo {
 		Homininae gorilla = new Gorillini();
 		Homininae gorilla2 = new Gorillini();
 		Homininae gorilla3 = new Gorillini();
-
-		//5 Homo
-		Homininae sapiens = new Homo('m');
+		//Homininae sapiens2;
 		Homininae sapiens2 = new Homo('f', "Haydee Orellana Farias");
 		Homininae neanderthalis = new Homo();
 		Homininae habilis = new Homo();
 		Homininae erectus = new Homo();
 
+		try {
+			sapiens2.setSex('x'); //set bogus exception
+		}
+		catch(SexException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			sapiens2.setHeight(0);
+		}
+		catch(HeightException e) {
+			e.printStackTrace();
+		}
+
 		//2 Australeopithecus
 		Homininae africanus = new Australeopithecus();
 		Homininae afarensis = new Australeopithecus();
-		
+			
 		print("Homininae population: " + Homininae.getPopulation());
 		print("Gorillini population: " + Gorillini.getPopulation());
 		print("Homini population: " + Homini.getPopulation());
 		print("Homo population: " + Homo.getPopulation());
 		print("Australeopithecus population: " + Australeopithecus.getPopulation());
 
-		print(((Homo)sapiens2).getName()); //cast Homo
+		//print(((Homo)sapiens2).getName()); //cast Homo
 		print(sapiens2.getSex());
 
 
