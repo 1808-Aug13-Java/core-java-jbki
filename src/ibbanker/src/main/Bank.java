5f6d9e575b9aaec833de7fbcd7ff769d165f008c
Jerry <jbki@protonmail.com>
2018-08-20 12:22:18 -0400

    InputMismatchException; Bank helper class

--- src/ibbanker/src/main/Bank.java
+++ src/ibbanker/src/main/Bank.java
@@ -1,7 +1,174 @@
 package main;
 import java.io.Console;
+import java.util.*;
+import java.io.*;
+
 
 public class Bank {
+  public static final String CREATE_USR = "create username: ";
+  static final String CREATE_PWD = "create password: ";
+  static final String USR_TAKEN = "username taken";
+  static final String GET_USR = "enter username: "; 
+  static final String GET_PWD = "enter password: ";
+  static final String DEPOSIT_AMT = "enter deposit amount: ";
+  static final String WITHDRAW_AMT = "enter withdrawal amount: ";
+  static final String DELIMIT = " "; //delimiter used by scanner to parse user files
+  static final String BALANCE = "balance";
+  public static void print(Object o) {
+    System.out.println(o);
+  }
+  static void view(File f) throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(f));
+    //BufferedReader reader = Files.newBufferedReader(f.toPath());
+    Scanner sc = new Scanner(reader);
+    //String BALANCE = "balance:"; //scanner returns token string, which is searched for with contains
+    while(sc.hasNext())
+    {
+      try {
+        String temp = sc.next();
+        //System.out.print("temp="+temp);
+        if(temp.contains(BALANCE)) 
+        {
+          float balance = sc.nextFloat();
+          System.out.println("balance: " + balance);
+        }
+      }
+      catch(InputMismatchException e) {
+        continue;
+        //System.out.println("HEYO");
+        //e.printStackTrace();
+      }
+      catch(NoSuchElementException e) {
+        e.printStackTrace();
+        break;
+      }
+    }
+    reader.close();
+  }
+  public static boolean verify(String u, String p) {
+    
+    return true;
+  }
+  static String createUsername(String msg) {
+    Console c = System.console();
+    return c.readLine(msg);
+  }
+//TODO throw username taken error
+  static String createPassword(String msg) {
+    Console c = System.console();
+    System.out.println(msg);
+    return c.readPassword().toString();
+  }
+  static boolean usernameTaken(String username, String msg) {
+    boolean taken = false;
+    if(taken) {
+      System.out.print(msg);
+      return taken; //taken = true
+    }
+    return taken; //taken = false
+  }
+
+  static String getUsername(String msg) {
+    Console c = System.console();
+    return c.readLine(msg);
+  }
+  static String getPassword(String msg) {
+    Console c = System.console();
+    System.out.println(msg);
+    return c.readPassword().toString();
+  }
+
+  static void showTransactionsMenu() {
+    System.out.println("1. deposit");
+    System.out.println("2. withdraw");
+    System.out.println("3. view");
+    System.out.println("4. logout");
+  }
+  static Scanner getScanLine(Scanner sc, String target) {
+    while(sc.hasNext())
+    {
+      try {
+        if(sc.next().contains(target)) 
+        {
+          return sc;
+        }
+        else continue;
+      }
+      catch(NoSuchElementException e) {
+        e.printStackTrace();
+        break;
+      }
+    }
+    return sc;
+  }
+  static void deposit(File f, float amount) throws IOException {
+    String contents = "";
+    BufferedReader reader = new BufferedReader(new FileReader(f));
+    BufferedWriter writer = null; 
+    String line = "";
+    Scanner sc = new Scanner(reader);
+
+    while((line = reader.readLine()) != null) {
+      if(line.contains(BALANCE)) {
+        String[] lines = line.split(DELIMIT);
+        float curBalance = Float.parseFloat(lines[1]);
+        float newBalance = curBalance + amount; 
+        line = lines[0] + DELIMIT + Float.toString(newBalance); 
+      }
+      contents += line + "\n";
+    }
+    reader.close();
+    sc.close();
+     
+    
+    writer = new BufferedWriter(new FileWriter(f));
+    writer.write(contents);
+    //TODO put in finally 
+    writer.close();
+  }
+
+  static boolean withdraw(File f, float amount) throws IOException, NegativeFloatException {
+    String contents = "";
+    BufferedReader reader = new BufferedReader(new FileReader(f));
+    BufferedWriter writer = null; 
+    String line = "";
+    Scanner sc = new Scanner(reader);
+
+    try {
+      while((line = reader.readLine()) != null) {
+        if(line.contains(BALANCE)) {
+          String[] lines = line.split(DELIMIT);
+          float curBalance = Float.parseFloat(lines[1]);
+          float newBalance = curBalance - amount; 
+          if(curBalance < amount) {
+            throw new NegativeFloatException();
+          }
+                        
+          line = lines[0] + DELIMIT + Float.toString(newBalance); 
+        }
+        contents += line + "\n";
+      }
+    }
+    catch(NegativeFloatException e) {
+      System.out.println("You don't have enough balance");
+      return false;
+    }
+    finally {
+      reader.close();
+      sc.close();
+    }
+     
+    
+    writer = new BufferedWriter(new FileWriter(f));
+    writer.write(contents);
+    //TODO put in finally 
+    writer.close();
+    return true;
+  }
+
+
+  static void logout(File f) {
+  }
 }
 
 
--- src/ibbanker/src/main/BankDriver.java
+++ src/ibbanker/src/main/BankDriver.java
@@ -8,17 +8,9 @@ import java.io.FileReader;
 import java.nio.file.*;
 import java.nio.*;
 import java.nio.file.StandardOpenOption;
+import main.Bank.*;
 class NegativeFloatException extends RuntimeException {}
 public class BankDriver {
-  static final String CREATE_USR = "create username: ";
-  static final String CREATE_PWD = "create password: ";
-  static final String USR_TAKEN = "username taken";
-  static final String GET_USR = "enter username: "; 
-  static final String GET_PWD = "enter password: ";
-  static final String DEPOSIT_AMT = "enter deposit amount: ";
-  static final String WITHDRAW_AMT = "enter withdrawal amount: ";
-  static final String BALANCE = "balance";
-  static final String DELIMIT = " "; //delimiter used by scanner to parse user files
   public static void main(String ... args) {
     float amount = 0;
     while(true) {
@@ -32,76 +24,73 @@ public class BankDriver {
       switch (select) {
         case 1: 
                 do {
-                  username = createUsername(CREATE_USR);
-                } while(usernameTaken(username, USR_TAKEN));
-                password = createPassword(CREATE_PWD);
+                  username = Bank.createUsername(Bank.CREATE_USR);
+                } while(Bank.usernameTaken(username, Bank.USR_TAKEN));
+                password = Bank.createPassword(Bank.CREATE_PWD);
                 
                 try {
                   File file = new File(username);
                   file.createNewFile();
                   BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-                  writer.write("username: " + username + '\n');
+                  writer.write("username: " + username.toString() + '\n');
                   writer.write("password: " + password + '\n');
-                  writer.write("balance: " + password + '\n');
+                  writer.write("balance: " + "0" + '\n');
                   writer.close();
                 }
                 catch(FileNotFoundException e) {
                   e.printStackTrace();
                 }
-                catch(IOException e) {
-                  //do something
-                }
                 catch(Exception e) {
-                  //do smething
+                  e.printStackTrace();
                 }
                 break;
         case 2:
-                //TODO Uncomment
-                //username = getUsername(GET_USR); 
-                //password = getPassword(GET_PWD); 
-                username = "jbki";
-                System.out.println("");
+               username = Bank.getUsername(Bank.GET_USR); 
+               password = Bank.getPassword(Bank.GET_PWD); 
+               // username = "jbki";
+               // System.out.println("");
                 //catch bad login attempts
-                if(!(verify(username, password))) {
+                if(!(Bank.verify(username, password))) {
                   System.out.println("login failed");
                 }
 
                 //logged in, show menu 
-                try {
                   boolean loggedin = true;
+                  File file = new File(username); //retrieve file of the username
+                                      
                   while(loggedin) {
-                    File file = new File(username); //retrieve file of the username
-                    showTransactionsMenu();
-                    select = Integer.parseInt(c.readLine()); 
-                    switch (select) {
-                      case 1: 
-                        amount = Float.parseFloat(c.readLine(DEPOSIT_AMT));
-                        deposit(file, amount);
-                        break;
-                      case 2:
-                        do {
-                          amount = Float.parseFloat(c.readLine(WITHDRAW_AMT));
-                        } while(!withdraw(file, amount));
-                        break;
-                      case 3:
-                        view(file);
-                        break;
-                      case 4:
-                        logout(file);
-                        loggedin = false;
-                        break;
-                      default:
-                       System.out.println("don't know that option"); 
+                    Bank.showTransactionsMenu();
+                    try {
+                      select = Integer.parseInt(c.readLine());  //getting input
+                      switch (select) {
+                        case 1: 
+                          amount = Float.parseFloat(c.readLine(Bank.DEPOSIT_AMT));
+                          Bank.deposit(file, amount);
+                          break;
+                        case 2:
+                          do {
+                            amount = Float.parseFloat(c.readLine(Bank.WITHDRAW_AMT));
+                          } while(!Bank.withdraw(file, amount));
+                          break;
+                        case 3:
+                          Bank.view(file);
+                          break;
+                        case 4:
+                          Bank.logout(file);
+                          loggedin = false;
+                          break;
+                        default:
+                         System.out.println("don't know that option"); 
+                      }
+                    }
+                    catch(NumberFormatException e) {
+                      System.out.println("bad input");
+                      continue;
+                    }
+                    catch(IOException e) {
+                      e.printStackTrace();
                     }
                   }
-                }
-                catch(IOException e) {
-                  e.printStackTrace();
-                }
-                finally {
-                  System.out.println("");
-                }
-                break;
         case 3: 
           System.out.println("bye");
           System.exit(0);
@@ -111,150 +100,6 @@ public class BankDriver {
     }
   }
 
-  public static boolean verify(String u, String p) {
-    return true;
-  }
-  protected static String createUsername(String msg) {
-    Console c = System.console();
-    return c.readLine(msg);
-  }
-//TODO throw username taken error
-  protected static String createPassword(String msg) {
-    Console c = System.console();
-    System.out.println(msg);
-    return c.readPassword().toString();
-  }
-  protected static boolean usernameTaken(String username, String msg) {
-    boolean taken = false;
-    if(taken) {
-      System.out.print(msg);
-      return taken; //taken = true
-    }
-    return taken; //taken = false
-  }
-
-  protected static String getUsername(String msg) {
-    Console c = System.console();
-    return c.readLine(msg);
-  }
-  protected static String getPassword(String msg) {
-    Console c = System.console();
-    System.out.println(msg);
-    return c.readPassword().toString();
-  }
-
-  protected static void showTransactionsMenu() {
-    System.out.println("1. deposit");
-    System.out.println("2. withdraw");
-    System.out.println("3. view");
-    System.out.println("4. logout");
-  }
-  protected static Scanner getScanLine(Scanner sc, String target) {
-    while(sc.hasNext())
-    {
-      try {
-        if(sc.next().contains(target)) 
-        {
-          return sc;
-        }
-        else continue;
-      }
-      catch(NoSuchElementException e) {
-        e.printStackTrace();
-        break;
-      }
-    }
-    return sc;
-  }
-  protected static void deposit(File f, float amount) throws IOException {
-    String contents = "";
-    BufferedReader reader = new BufferedReader(new FileReader(f));
-    BufferedWriter writer = null; 
-    String line = "";
-    Scanner sc = new Scanner(reader);
-
-    while((line = reader.readLine()) != null) {
-      if(line.contains(BALANCE)) {
-        String[] lines = line.split(DELIMIT);
-        float curBalance = Float.parseFloat(lines[1]);
-        float newBalance = curBalance + amount; 
-        line = lines[0] + DELIMIT + Float.toString(newBalance); 
-      }
-      contents += line + "\n";
-    }
-    reader.close();
-    sc.close();
-     
-    
-    writer = new BufferedWriter(new FileWriter(f));
-    writer.write(contents);
-    //TODO put in finally 
-    writer.close();
-  }
-
-  protected static boolean withdraw(File f, float amount) throws IOException, NegativeFloatException {
-    String contents = "";
-    BufferedReader reader = new BufferedReader(new FileReader(f));
-    BufferedWriter writer = null; 
-    String line = "";
-    Scanner sc = new Scanner(reader);
-
-    try {
-      while((line = reader.readLine()) != null) {
-        if(line.contains(BALANCE)) {
-          String[] lines = line.split(DELIMIT);
-          float curBalance = Float.parseFloat(lines[1]);
-          float newBalance = curBalance - amount; 
-          if(curBalance < amount) {
-            throw new NegativeFloatException();
-          }
-                        
-          line = lines[0] + DELIMIT + Float.toString(newBalance); 
-        }
-        contents += line + "\n";
-      }
-    }
-    catch(NegativeFloatException e) {
-      System.out.println("You don't have enough balance");
-      return false;
-    }
-    finally {
-      reader.close();
-      sc.close();
-    }
-     
-    
-    writer = new BufferedWriter(new FileWriter(f));
-    writer.write(contents);
-    //TODO put in finally 
-    writer.close();
-    return true;
-  }
-
-  protected static void view(File f) throws IOException {
-    BufferedReader reader = Files.newBufferedReader(f.toPath());
-    Scanner sc = new Scanner(reader);
-    String BALANCE = "balance:"; //scanner returns token string, which is searched for with contains
-    while(sc.hasNext())
-    {
-      try {
-        if(sc.next().contains(BALANCE)) 
-        {
-          int balance = sc.nextInt();
-          System.out.println("balance: " + balance);
-        }
-        else continue;
-      }
-      catch(NoSuchElementException e) {
-        e.printStackTrace();
-        break;
-      }
-    }
-    reader.close();
-  }
-
-  protected static void logout(File f) {
-  }
 
 
 }
